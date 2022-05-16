d59cf64b003d64fe771389c63364d85f40a6c49b
Frauke Albrecht <albrecht@dkrz.de>
2022-05-16 07:33:21 +0200

    week 10 kubernetes

---
+++ week10/test.py
@@ -0,0 +1,8 @@
+import requests
+url = 'http://localhost:9696/predict'
+#url = 'http://localhost:8080/predict'
+
+data = {'url': 'http://bit.ly/mlbookcamp-pants'}
+
+result = requests.post(url, json=data).json()
+print(result)
