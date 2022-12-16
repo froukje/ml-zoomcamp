4e0d3bceec7afb14af62d28a0dea4138712359d3
Frauke Albrecht <albrecht@dkrz.de>
2022-12-16 09:06:26 +0100

    deploy to aws

--- week10/test.py
+++ week10/test.py
@@ -1,6 +1,7 @@
 import requests
 #url = 'http://localhost:9696/predict'
-url = 'http://localhost:8080/predict'
+#url = 'http://localhost:8080/predict'
+url = 'http://af6a03b14e6834fa9b77454bfbdf400a-1093699068.eu-west-1.elb.amazonaws.com/predict'
 
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
